99cab2efde903e72e8ffeba72e4e6194e2a7a0fa
Alvaro Segura <asegura0212@outlook.com>
2021-04-05 07:52:34 -0500

    Inicial

---
+++ ActaulizarSeven/Controllers/IndexController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActaulizarSeven.Controllers
+{
+    public class IndexController : Controller
+    {
+        // GET: Index
+        public ActionResult Index()
+        {
+            int a = 1;
+            string b;
+            b = a.ToString();
+            return View();
+        }
+    }
+}
