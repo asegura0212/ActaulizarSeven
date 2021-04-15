4f77c73b776c2fcc04d536665b9054e12a363cfa
Alvaro Segura <asegura0212@outlook.com>
2021-04-15 11:27:56 -0500

    Creación clase usuarios

---
+++ ActaulizarSeven/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ActaulizarSeven.Models
+{
+    public class Usuario
+    {
+        int idUsuario { get; set; }
+        string nombreUsuario { get; set; }
+
+
+    }
+}
